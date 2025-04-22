f8d49850e0686b99ff917d32854dbbbaafe9367a
Breno <brenomelogabriel2019@gmail.com>
2025-04-22 17:19:55 -0300

    feat(auth): wrap reset password form in Suspense for improved loading handling

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import ResetForm from "@/components/reset-password-form";
+import { Suspense } from 'react'
+
 
 export default function RecoverPasswordPage() {
   return (
     <div className="grid min-h-screen place-items-center">
-        <ResetForm/>
+        <Suspense>
+          <ResetForm/>
+        </Suspense>
     </div>
   );
 }
